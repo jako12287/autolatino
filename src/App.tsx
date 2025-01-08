5793cde5f5defcb7e345cc0087f41fd77ff62dfc
Johan Cortes <jako12287@gmail.com>
2025-01-08 18:28:06 -0500

    fisrt commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import images from "../src/assets/Carroceria-58.jpg"
+
+function App() {
+  return (
+    <>
+      <h1>AUTOLATINO</h1>
+      <img src={images}/>
+    </>
+  );
+}
+
+export default App;
