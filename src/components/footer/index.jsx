fd53d7f96d010c0f82b9ca6dbe29631bf7de4047
Johan Cortes <jako12287@gmail.com>
2025-03-20 14:21:12 -0500

    fixed layout in footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
   return (
     <div className="sm:min-h-[350px] bg-black flex flex-col">
       <div className="flex-2 flex flex-col md:flex-row justify-between p-8">
-        <div className="flex items-center">
+        <div className="w-full flex items-center md:border-r-2 border-white-50">
           <ul className=" flex flex-col  text-white-50 cursor-pointer">
             {itemNav.map((item) => (
               <NavLink
@@ -29,14 +29,14 @@ const Footer = () => {
             ))}
           </ul>
         </div>
-        <div className="flex justify-center items-center">
+        <div className="w-full flex items-center  justify-center">
           <img
             src={LogoAutolatino}
             className="h-14 w-auto object-contain"
             alt="Logo autolatino"
           />
         </div>
-        <div className="text-white flex flex-col gap-4 justify-center">
+        <div className="w-full flex md:border-l-2 border-white-50 text-white flex-col gap-4 justify-center pl-2 md:pl-10">
           <h3 className="text-customred font-bold">Cantacto:</h3>
           <div>
             <img
@@ -48,7 +48,7 @@ const Footer = () => {
           <span className="flex items-center gap-1">
             <img
               src={Mail}
-              className="w-auto object-contain"
+              className="w-auto object-contain object-center"
               alt="Logo autolatino"
             />
             <a 
